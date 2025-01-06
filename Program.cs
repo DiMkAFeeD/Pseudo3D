649df286c5acb782ab69f2f91355396c5fcf0f9b
dimkafeed <banshchikov.2021@bk.ru>
2025-01-06 13:53:22 +0500

    create SFML init

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
 
-namespace Pseudo3D
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // ИНИЦИАЛИЗАЦИЯ ОКНА
+
+        RenderWindow window = new RenderWindow(new VideoMode(800, 600), "SFML 3.0 C# Example");
+        View view = window.GetView();
+
+        // ИНИЦИАЛИЗАЦИЯ ОБЪЕКТОВ
+
+       
+        // ОБРАБОТКА СОБЫТИЙ
+
+        window.Closed += (sender, e) =>
         {
-            Console.WriteLine("Hello, World!");
+            window.Close();
+            Console.WriteLine("Close");
+        };
+
+        window.Resized += (sender, e) =>
+        {
+            view.Reset(new FloatRect(0, 0, e.Width, e.Height));
+        };
+
+        // ЛОГИКА ОКНА 
+
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+
+            if (window.HasFocus())
+            {
+                
+            }
+
+            window.SetView(view);
+            window.Clear();
+
+            window.Display();
         }
     }
 }
